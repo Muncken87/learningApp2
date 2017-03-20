3e15d6c7720a3edd88e8d1f56bfad18595f90907
TahirKalim <thrkalim@gmail.com>
2017-03-20 20:23:35 +0100

    made changes for 4.3 controller & routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
   end
-end
+
 
   def landing_page
     @featured_product = Product.first
+  end
+
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
 
   get 'static_pages/contact'
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'static_pages/index'
 	
 	root 'static_pages#landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
