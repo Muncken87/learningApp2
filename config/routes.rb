a81655795c31e48e0fb0b680e7c7ef2d9a94686e
TahirKalim <thrkalim@gmail.com>
2017-03-30 04:24:30 +0200

    added 3rd time color field to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :products, :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
 
--- db/migrate/20170330011446_add_color_to_products.rb
+++ db/migrate/20170330011446_add_color_to_products.rb
@@ -1,7 +1,5 @@
-class AddColourToProducts < ActiveRecord::Migration[5.0]
+class AddColorToProducts < ActiveRecord::Migration[5.0]
   def change
     add_column :products, :color, :string
   end
 end
-
-
